57dcc5fae83933f2f63a6f78cb8d283b0f2b4f61
jsaique <jsaique11@gmail.com>
2023-02-04 00:10:42 -0500

    Setting the routing and fetching data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 // Components
 import NavbarComponent from "./navbar";
 import HomeComponent from "./home";
@@ -8,20 +8,32 @@ import CrewComponent from "./crew";
 import TechnologyComponent from "./technology";
 
 const App = () => {
+  // Fetching the data
+  const URL =
+    "https://raw.githubusercontent.com/jsaique/bugify/master/data.json";
+
+  const getData = async () => {
+    const res = await fetch(URL);
+    const data = await res.json();
+    return data;
+  };
+
+  const location = useLocation().pathname;
+  const newClass = location.split("/")[1];
   return (
-    <div className="menu">
-      <NavbarComponent />
+    <div className={`main ${newClass}`}>
+      <NavbarComponent currentRoute={newClass} />
       <Route exact path="/">
         <HomeComponent />
       </Route>
       <Route path="/destination">
-        <DestinationComponent />
+        <DestinationComponent data={getData()} />
       </Route>
       <Route path="/crew">
-        <CrewComponent />
+        <CrewComponent data={getData()} />
       </Route>
       <Route path="/technology">
-        <TechnologyComponent />
+        <TechnologyComponent data={getData()} />
       </Route>
     </div>
   );
